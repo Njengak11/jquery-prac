ba569543c410b106b30cc126ff1c9139d0d98e11
njenga1011 <njenga.k11@gmail.com>
2018-07-04 17:19:08 +0300

    js folder

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $("h1").click(function() {
+    alert("This is a header.");
+    alert("I told you this si a header!")
+  });
+
+  $("p").click(function() {
+    alert("This is a paragraph.");
+  });
+
+  $("img").click(function() {
+    alert("This is an image.");
+  });
+});
